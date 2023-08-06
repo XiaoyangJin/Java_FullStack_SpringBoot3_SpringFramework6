aa8ba5c87c6b3727ce416522eced163d812607bf
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-08-06 19:03:17 -0400

    intro to spring

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -12,8 +12,15 @@ public class AppGamingBasicJava {
 		
 		//var game = new MarioGame();
 		//var game = new SuperContraGame();
-		var game = new PacmanGame();
+		
+		var game = new PacmanGame(); //1: Object Creation
 		var gameRunner = new GameRunner(game);
+			//2: Object Creation + Wiring of Dependencies
+			//a GameRunner class needs to be able to run a game -> Game is a Dependency
+			//a GameConsole is a Dependency of a GameRunner class
+			//so 'new GameRunner(game)' means game is injected/wired into the GameRunner class
+			//Spring can help us to do the create and wire things
+		
 		gameRunner.run();
 
 	}
