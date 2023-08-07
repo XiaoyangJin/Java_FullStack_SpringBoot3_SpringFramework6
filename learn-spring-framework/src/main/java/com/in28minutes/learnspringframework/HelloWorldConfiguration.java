b7b8e602c61b3f4a05508a56ad31ca63b5d8c259
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-08-06 20:54:32 -0400

    more Beans & record

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,6 +3,14 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person (String name, int age) {
+	//record means we don't need to build getters, setters, constructor..., it already automatically created for us
+	
+}
+record Address (String firstLine, String city) {
+	
+}
+
 @Configuration
 public class HelloWorldConfiguration {
 	
@@ -11,4 +19,23 @@ public class HelloWorldConfiguration {
 	public String name() {
 		return "Mia";
 	}
+	
+	@Bean
+	public Integer age() {
+		return 25;
+	}
+	
+	@Bean
+	public Person person() {
+		//var person = new Person("Jay", 56);
+		//person.name();
+		//return person;
+		return new Person("Jay", 56);
+	}
+	
+	@Bean
+	public Address address() {
+		return new Address("Baker street", "London");
+	}
+	
 }
