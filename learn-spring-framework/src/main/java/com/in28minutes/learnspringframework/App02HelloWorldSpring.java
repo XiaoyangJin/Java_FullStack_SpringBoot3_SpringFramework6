39fd2464e9aaeeb463bc74ae96a58ce617881c21
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-08-06 20:37:10 -0400

    launch a spring context using a configuration file

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -0,0 +1,21 @@
+package com.in28minutes.learnspringframework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.MarioGame;
+import com.in28minutes.learnspringframework.game.PacmanGame;
+import com.in28minutes.learnspringframework.game.SuperContraGame;
+
+public class App02HelloWorldSpring {
+
+	public static void main(String[] args) {
+		//1: Launch a Spring Context -
+		
+		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+		
+		//2: Configure the things that we want Spring to manage - @Configuration
+
+	}
+
+}
