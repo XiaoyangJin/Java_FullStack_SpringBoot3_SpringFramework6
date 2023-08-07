f83d89f1e25f8fa4eefaa3e52840d70bb1d87022
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-08-06 20:43:15 -0400

    name Bean

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -1,8 +1,14 @@
 package com.in28minutes.learnspringframework;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HelloWorldConfiguration {
 	
+	//this Bean will be managed by this spring container
+	@Bean
+	public String name() {
+		return "Mia";
+	}
 }
