779490ede1688d9cf9edb8b3f7ae66e4d5dc6709
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-08-08 10:04:07 -0400

    setter injection

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
@@ -14,14 +14,32 @@ import org.springframework.stereotype.Component;
 //Dependency2
 @Component
 class YourBusinessClass{
-	@Autowired
+	
+	//field injection
+	
 	Dependency1 dependency1;
-	@Autowired
+	
 	Dependency2 dependency2;
+	
+	//setter injection
+	@Autowired
+	public void setDependency1(Dependency1 dependency1) {
+		System.out.println("Setter Injection - setDependency1");
+		this.dependency1 = dependency1;
+	}
+
+	@Autowired
+	public void setDependency2(Dependency2 dependency2) {
+		System.out.println("Setter Injection - setDependency2");
+		this.dependency2 = dependency2;
+	}
+	
 	
 	public String toString() {
 		return "Using " + dependency1 + " and " + dependency2;
 	}
+	
+	
 }
 
 @Component
