0c78abf25e8587ad66c66b9542846516a18564ee
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-08-08 09:43:29 -0400

    set up for dependency injection
    in fact we don't have to add ("com.in28minutes.learnspringframework.examples.a1") to @ComponentScan because it will automatically scan in the same package

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
@@ -1,23 +1,20 @@
-package com.in28minutes.learnspringframework;
+package com.in28minutes.learnspringframework.examples.a1;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import com.in28minutes.learnspringframework.game.GameConsole;
-import com.in28minutes.learnspringframework.game.GameRunner;
+//dependency injection
 
 @Configuration
-@ComponentScan
-public class GamingAppLauncherApplication {
+@ComponentScan("com.in28minutes.learnspringframework.examples.a1")
+public class DepInjectionLauncherApplication {
 
 	public static void main(String[] args) {
 		
 		try(var context = new AnnotationConfigApplicationContext
-				(GamingAppLauncherApplication.class)){
-			context.getBean(GameConsole.class).up();
-			context.getBean(GameRunner.class).run();
+				(DepInjectionLauncherApplication.class)){
+			
 		}
 
 	}
