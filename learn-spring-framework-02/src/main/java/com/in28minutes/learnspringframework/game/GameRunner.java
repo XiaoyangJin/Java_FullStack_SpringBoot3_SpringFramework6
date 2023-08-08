b5c2d5f5c8e2509f9c7b4c54eafa89bd33119156
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-08-08 09:34:09 -0400

    qualifier using

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
@@ -1,5 +1,6 @@
 package com.in28minutes.learnspringframework.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,7 +8,7 @@ public class GameRunner {
 	//create an instance of MarioGame
 	private GameConsole game;
 	
-	public GameRunner(GameConsole game) {
+	public GameRunner(@Qualifier("SuperContraGameQualifier")GameConsole game) {
 		this.game = game;
 	}
 
