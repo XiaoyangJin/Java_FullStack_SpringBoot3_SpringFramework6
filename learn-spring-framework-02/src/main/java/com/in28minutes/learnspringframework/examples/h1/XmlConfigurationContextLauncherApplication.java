1f2e0110b775356521eebd2447a1b399dc452c6d
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-08-09 19:29:40 -0400

    xml beans

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/h1/XmlConfigurationContextLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/h1/XmlConfigurationContextLauncherApplication.java
@@ -19,6 +19,8 @@ public class XmlConfigurationContextLauncherApplication {
 			
 			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 			
+			System.out.println(context.getBean("name"));
+			System.out.println(context.getBean("age"));
 		}
 
 	}
