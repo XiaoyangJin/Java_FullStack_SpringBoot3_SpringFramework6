67d487c4aed946478bf27ba5a7ea1e2b5545a8df
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-08-09 19:26:39 -0400

    use xml configuration
    instead of java configuration(@Configuration)

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/h1/XmlConfigurationContextLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/h1/XmlConfigurationContextLauncherApplication.java
@@ -6,16 +6,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
-@Configuration
-@ComponentScan
+//@Configuration //java configuration
+//@ComponentScan
 public class XmlConfigurationContextLauncherApplication {
 
 	public static void main(String[] args) {
 		
-		try(var context = new AnnotationConfigApplicationContext
-				(XmlConfigurationContextLauncherApplication.class)){
+		try(var context = new ClassPathXmlApplicationContext("contextCondifuration.xml")){
 			
 			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 			
