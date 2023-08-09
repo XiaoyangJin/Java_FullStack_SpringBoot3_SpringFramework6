a7a58831423ef1be434e6e81f56e826de889218b
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-08-09 17:17:46 -0400

    Some Initialization logic

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/d1/LazyInitializationContextLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/d1/LazyInitializationContextLauncherApplication.java
@@ -1,4 +1,4 @@
-package com.in28minutes.learnspringframework.examples.a0;
+package com.in28minutes.learnspringframework.examples.d1;
 
 import java.util.Arrays;
 
@@ -6,16 +6,37 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 //dependency injection
+
+@Component
+class ClassA {
+	
+}
+
+@Component
+class ClassB {
+	
+	private ClassA classA;
+	
+	//classB is using classA bean to initialize itself
+	public ClassB(ClassA classA) {
+		//Logic
+		System.out.println("Some Initialization logic");
+		this.classA = classA;
+	}
+	
+}
+
 
 @Configuration
 @ComponentScan
-public class SimpleSpringContextLauncherApplication {
+public class LazyInitializationContextLauncherApplication {
 
 	public static void main(String[] args) {
 		
 		try(var context = new AnnotationConfigApplicationContext
-				(SimpleSpringContextLauncherApplication.class)){
+				(LazyInitializationContextLauncherApplication.class)){
 			
 			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 			
