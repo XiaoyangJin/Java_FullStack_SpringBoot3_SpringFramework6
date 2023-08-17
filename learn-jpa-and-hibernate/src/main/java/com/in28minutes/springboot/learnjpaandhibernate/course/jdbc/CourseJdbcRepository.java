810e384ca2e382a8e959211ab419be16905cb0a8
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-08-16 22:41:32 -0400

    execute query

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,11 +1,13 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CourseJdbcRepository {
 
+	@Autowired
 	private JdbcTemplate springJdbcTemplate;
 	
 	private static String INSERT_QUERY = 
