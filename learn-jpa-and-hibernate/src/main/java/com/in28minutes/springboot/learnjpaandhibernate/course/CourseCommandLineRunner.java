2276ddb505ae50e72a71faa7cd084ccd3cb814e5
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-08-20 11:42:05 -0400

    easiest springjpa

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -6,28 +6,34 @@ import org.springframework.stereotype.Component;
 
 import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner {
 
 //	@Autowired
 //	private CourseJdbcRepository repository;
-	
+
+//	@Autowired
+//	private CourseJpaRepository repository;
+
 	@Autowired
-	private CourseJpaRepository repository;
-	
+	private CourseSpringDataJpaRepository repository;
+
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		repository.insert(new Course(1, "Learn AWS Jpa", "in28minutes"));
-		repository.insert(new Course(2, "Learn Java Jpa", "in28minutes"));
-		repository.insert(new Course(3, "Learn SQL Jpa", "in28minutes"));
-		repository.insert(new Course(4, "Learn Python Jpa", "in28minutes"));
-		repository.insert(new Course(5, "Learn Spring Jpa", "in28minutes"));
+		repository.save(new Course(1, "Learn AWS Jpa", "in28minutes"));
+		repository.save(new Course(2, "Learn Java Jpa", "in28minutes"));
+		repository.save(new Course(3, "Learn SQL Jpa", "in28minutes"));
+		repository.save(new Course(4, "Learn Python Jpa", "in28minutes"));
+		repository.save(new Course(5, "Learn Spring Jpa", "in28minutes"));
+
+		repository.deleteById(3l);
+
+		System.out.println(repository.findById((long) 2));
+		System.out.println(repository.findById((long) 1));
 		
-		repository.deleteById((long)3);
-		System.out.println(repository.findById((long)2));
-		System.out.println(repository.findById((long)1));
 	}
 
 }
