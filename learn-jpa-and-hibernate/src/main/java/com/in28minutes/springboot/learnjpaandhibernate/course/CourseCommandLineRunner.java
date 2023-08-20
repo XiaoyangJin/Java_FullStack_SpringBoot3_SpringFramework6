006590ba98fd4db3c79c6f50ae9f4d2ddf7d21cb
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-08-20 11:42:39 -0400

    just mark it

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -23,11 +23,11 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		repository.save(new Course(1, "Learn AWS Jpa", "in28minutes"));
-		repository.save(new Course(2, "Learn Java Jpa", "in28minutes"));
-		repository.save(new Course(3, "Learn SQL Jpa", "in28minutes"));
-		repository.save(new Course(4, "Learn Python Jpa", "in28minutes"));
-		repository.save(new Course(5, "Learn Spring Jpa", "in28minutes"));
+		repository.save(new Course(1, "Learn AWS SpringJpa", "in28minutes"));
+		repository.save(new Course(2, "Learn Java SpringJpa", "in28minutes"));
+		repository.save(new Course(3, "Learn SQL SpringJpa", "in28minutes"));
+		repository.save(new Course(4, "Learn Python SpringJpa", "in28minutes"));
+		repository.save(new Course(5, "Learn Spring SpringJpa", "in28minutes"));
 
 		repository.deleteById(3l);
 
