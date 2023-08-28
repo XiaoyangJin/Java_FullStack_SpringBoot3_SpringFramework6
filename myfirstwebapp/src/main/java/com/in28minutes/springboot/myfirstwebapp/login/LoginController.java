0291acc9f99b4d44feb01a17ba63249ea011b59d
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-08-28 11:26:51 -0400

    prepare for login page

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,17 +1,14 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 	
 	//http://localhost:8080/login?name=summer
 	@RequestMapping("login")
-	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
-		model.put("name", name);
+	public String gotoLoginPage() {
 		
 		return "login";
 	}
