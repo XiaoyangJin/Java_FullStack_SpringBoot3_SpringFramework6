dd8eec111cda3476c216e74c43ca2c3a065937d0
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-09-04 16:19:28 -0400

    manage time & format date

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -39,7 +39,7 @@ public class TodoController {
 	@RequestMapping(value = "add-todo", method = RequestMethod.GET)
 	public String showNewTodoPage(ModelMap model) {
 		String username = (String)model.get("name");
-		Todo todo = new Todo(0, username, "Default Description", LocalDate.now().plusYears(1), false);
+		Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
 		model.put("todo", todo);
 		return "todo";
 	}
@@ -63,10 +63,23 @@ public class TodoController {
 		return "redirect:list-todos";
 	}
 	
-	@RequestMapping(value = "update-todo")
+	@RequestMapping(value = "update-todo", method = RequestMethod.GET)
 	public String showUpdateTodoPage(@RequestParam int id, ModelMap model) {
 		Todo todo = todoService.findById(id);
 		model.addAttribute("todo", todo);
 		return "todo";
+	}
+	
+	@RequestMapping(value = "update-todo", method = RequestMethod.POST)
+	public String updateTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "todo";
+		}
+		
+		String username = (String)model.get("name");
+		todo.setUserName(username);
+		todoService.updateTodo(todo);
+		return "redirect:list-todos";
 	}
 }
