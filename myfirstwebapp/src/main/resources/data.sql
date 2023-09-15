134df5730113ac9f31c80e1001c71cf4fb78c0ee
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-09-15 14:50:50 -0400

    some hard data

--- myfirstwebapp/src/main/resources/data.sql
+++ myfirstwebapp/src/main/resources/data.sql
@@ -1,2 +1,11 @@
 insert into todo(ID, USER_NAME, DESCRIPTION, TARGET_DATE, DONE)
 values(10001, 'Lin', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo(ID, USER_NAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002, 'Lin', 'Get Azure Certified', CURRENT_DATE(), false);
+
+insert into todo(ID, USER_NAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003, 'Lin', 'Get DevOps Certified', CURRENT_DATE(), false);
+
+insert into todo(ID, USER_NAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004, 'Lin', 'Get GCP Certified', CURRENT_DATE(), false);
