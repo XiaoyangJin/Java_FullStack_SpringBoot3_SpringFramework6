1e4c807374c1ee95fa468ec96529e12c84fbc09c
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-09-15 14:33:16 -0400

    connected

--- myfirstwebapp/src/main/resources/data.sql
+++ myfirstwebapp/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into todo(ID, USERNAME. DESCRIPTION, TARGET_DATE, DONE)
-values(10001, "Lin", "Get AWS Certified", CURRENT_DATE(), false);
+insert into todo(ID, USER_NAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001, 'Lin', 'Get AWS Certified', CURRENT_DATE(), false);
