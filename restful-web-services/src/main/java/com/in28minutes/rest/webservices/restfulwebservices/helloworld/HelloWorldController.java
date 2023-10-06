b957ae485f55947bc57740043e7e754e526fdce2
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-10-06 16:00:15 -0400

    pathvariable

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //REST API
@@ -20,4 +21,12 @@ public class HelloWorldController {
 		return new HelloWorldBean("Hello World");
 	}
 
+	// path parameters
+	// /users/{id}/todos/{id} => /users/200/todos/2
+	// /hello-world/path-variable/{name}
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name)); //%s => String format
+	}
 }
