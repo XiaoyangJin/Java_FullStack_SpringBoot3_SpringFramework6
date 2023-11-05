c4716f7e51c5dfc9594c727c8603243fbe83b245
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-05 16:17:54 -0500

    test Post link with h2

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -6,3 +6,15 @@ VALUES (CURRENT_DATE(), 10002, 'lin');
 
 INSERT INTO user_details (birth_date, id, name)
 VALUES (CURRENT_DATE(), 10003, 'ada');
+
+INSERT INTO post (id, user_id, description)
+VALUES (20001, 10001, 'I want to learn AWS');
+
+INSERT INTO post (id, user_id, description)
+VALUES (20002, 10001, 'I want to learn DevOps');
+
+INSERT INTO post (id, user_id, description)
+VALUES (20003, 10002, 'I want to get AWS Certified');
+
+INSERT INTO post (id, user_id, description)
+VALUES (20004, 10002, 'I want to learn cloud');
