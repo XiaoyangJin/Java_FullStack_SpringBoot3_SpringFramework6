437f89f5b4807aae7436f88959c75d823ee408d4
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-05 15:41:07 -0500

    double test

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -1,2 +1,8 @@
 INSERT INTO user_details (birth_date, id, name)
 VALUES (CURRENT_DATE(), 10001, 'summer');
+
+INSERT INTO user_details (birth_date, id, name)
+VALUES (CURRENT_DATE(), 10002, 'lin');
+
+INSERT INTO user_details (birth_date, id, name)
+VALUES (CURRENT_DATE(), 10003, 'ada');
