bcc031d08f4ae7080426e24f5678268c8c6d4ca4
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-05 15:49:06 -0500

    use repository instead of service
    JPA

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserJpaResource.java
@@ -0,0 +1,84 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.in28minutes.rest.webservices.restfulwebservices.jpa.UserRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+public class UserJpaResource {
+	
+	//autowire
+	private UserDaoService service;
+	
+	private UserRepository repository;
+	
+	//constructor
+	public UserJpaResource(UserDaoService service, UserRepository repository) {
+		this.service = service;
+		this.repository = repository;
+	}
+	
+	//GET /users
+	@GetMapping("/jpa/users")
+	public List<User> retrieveAllUsers(){
+		return repository.findAll();
+	}
+	
+	//http://localhost:8080/users
+	
+	//EntityModel
+	//WebMvcLinkBuilder
+	
+	@GetMapping("/jpa/users/{id}")
+	public EntityModel<User> retrieveUser(@PathVariable int id){
+		User user = service.findOne(id);
+		
+		if(user == null) {
+			throw new UserNotFoundException("id:" + id);
+		}
+		
+		EntityModel<User> entityModel = EntityModel.of(user);
+		//a link point to the controller
+		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		entityModel.add(link.withRel("all-users"));
+		
+		return entityModel;
+	}
+	
+	//delete user
+	@DeleteMapping("/jpa/users/{id}")
+	public void deleteUser(@PathVariable int id){
+		service.deleteById(id);
+	}
+	
+	//POST /users
+	@PostMapping("/jpa/users")
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+		User savedUser = service.save(user);
+		// /users/4 => /users/{id}, user.getID
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+			           .path("/{id}")
+				       .buildAndExpand(savedUser
+				       .getId())
+				       .toUri();
+		return ResponseEntity.created(location ).build();
+	}
+	
+}
