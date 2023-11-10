79af56c1c3b9895af41077dfc7d55bfd25033b85
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-10 10:24:01 -0500

    first component

--- Full-stack/frontend/todo-app/src/App.js
+++ Full-stack/frontend/todo-app/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 
+// All components we wrote shoud be child components of App component
 function App() {
   return (
     <div className="App">
       My Todo Application
+      <FirstComponent></FirstComponent>
     </div>
   );
+}
+
+function FirstComponent() {
+  return (
+    <div className="FirstComponent">First Component</div>
+  )
 }
 
 export default App;
