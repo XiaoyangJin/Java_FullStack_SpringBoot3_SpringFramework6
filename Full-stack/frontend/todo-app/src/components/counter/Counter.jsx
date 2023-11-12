9c94154ded16ae16e1b127cfe6c262ab210730de
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-12 11:45:15 -0500

    more attributes

--- Full-stack/frontend/todo-app/src/components/counter/Counter.jsx
+++ Full-stack/frontend/todo-app/src/components/counter/Counter.jsx
@@ -1,6 +1,14 @@
 export default function Counter() {
 
-    const buttonStyle = { fontSize: "30px" }
+    const buttonStyle = {
+        fontSize: "16px",
+        backgroundColor: "#00a5ab",
+        width: "100px",
+        margin: "10px",
+        color: "white",
+        padding: "15px",
+        borderRadius: "30px"
+    }
 
     function incrementCounterFunction() {
         console.log('increment clicked')
