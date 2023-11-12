9611f36ea89e1166f8790e64975de153f787169e
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-11 22:50:04 -0500

    correct one
    not auto default

--- Full-stack/frontend/todo-app/src/App.js
+++ Full-stack/frontend/todo-app/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import FirstComponent from './components/learning-examples/FirstComponent'
-import FifthComponent from './components/learning-examples/FirstComponent';
+import { FifthComponent } from './components/learning-examples/FirstComponent';
 import SecondComponent from './components/learning-examples/SecondComponent';
 import ThirdComponent from './components/learning-examples/ThirdComponent';
 import FourthComponent from './components/learning-examples/FourthComponent';
