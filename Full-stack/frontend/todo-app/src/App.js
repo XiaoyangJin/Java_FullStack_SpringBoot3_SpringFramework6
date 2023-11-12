1b9a280d54ef30dcf2bc2c5091593b59efa729be
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-11 22:46:23 -0500

    practice

--- Full-stack/frontend/todo-app/src/App.js
+++ Full-stack/frontend/todo-app/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { Component } from 'react';
 import FirstComponent from './components/learning-examples/FirstComponent'
+import SecondComponent from './components/learning-examples/SecondComponent';
+import ThirdComponent from './components/learning-examples/ThirdComponent';
+import FourthComponent from './components/learning-examples/FourthComponent';
 
 // All components we wrote shoud be child components of App component
 function App() {
@@ -12,35 +15,6 @@ function App() {
       <FourthComponent />
     </div>
   );
-}
-
-// function components
-function SecondComponent() {
-  return (
-    <div className="SecondComponent">Second Component</div>
-  )
-}
-
-// class components
-class ThirdComponent extends Component {
-  //method
-  render() {
-    return (
-      <div className="ThirdComponent">Third Component</div>
-    )
-  }
-}
-
-class FourthComponent extends Component {
-  //method
-  render() {
-    return (
-      <>
-        <div className="FourthComponent">Fourth Component</div>
-        <div className="FourthComponent">Fourth Component</div>
-      </>
-    )
-  }
 }
 
 export default App;
---
+++ Full-stack/frontend/todo-app/src/components/learning-examples/ThirdComponent.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react"
+export default class ThirdComponent extends Component {
+    //method
+    render() {
+        return (
+            <div className="ThirdComponent">Third Component</div>
+        )
+    }
+}
