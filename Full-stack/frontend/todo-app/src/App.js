d23e97bab3fb04b272b14aa171894853f13d0516
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-11 22:57:41 -0500

    combine all

--- Full-stack/frontend/todo-app/src/App.js
+++ Full-stack/frontend/todo-app/src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
-import FirstComponent from './components/learning-examples/FirstComponent'
-import { FifthComponent } from './components/learning-examples/FirstComponent';
-import SecondComponent from './components/learning-examples/SecondComponent';
-import ThirdComponent from './components/learning-examples/ThirdComponent';
-import FourthComponent from './components/learning-examples/FourthComponent';
+import LearningComponent from './components/learning-examples/LearningComponent';
 
 // All components we wrote shoud be child components of App component
 function App() {
   return (
     <div className="App">
-      <FirstComponent />
-      <SecondComponent />
-      <ThirdComponent />
-      <FourthComponent />
-      <FifthComponent />
+      <LearningComponent />
     </div>
   );
 }
