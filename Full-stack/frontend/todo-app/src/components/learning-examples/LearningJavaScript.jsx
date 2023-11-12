39a135d445201f1d47d5656f141b626c4e7560e8
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-11 23:02:42 -0500

    sub para

--- Full-stack/frontend/todo-app/src/components/learning-examples/LearningJavaScript.jsx
+++ Full-stack/frontend/todo-app/src/components/learning-examples/LearningJavaScript.jsx
@@ -1,13 +1,17 @@
 const person = {
     name: 'Summer',
-    address: '123 Baker Street, London'
+    address: {
+        line1: 'Baker Street',
+        city: 'London',
+        contry: 'UK',
+    }
 }
 
 export default function LearningJavaScript() {
     return (
         <div>
             <div>{person.name}</div>
-            <div>{person.address}</div>
+            <div>{person.address.city}</div>
         </div>
     );
 }
