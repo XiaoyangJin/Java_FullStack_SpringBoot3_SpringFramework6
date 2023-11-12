f3dd0a2b5c215b68094c4af750009a7908ce7f12
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-11 23:00:18 -0500

    const

--- Full-stack/frontend/todo-app/src/App.js
+++ Full-stack/frontend/todo-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import LearningComponent from './components/learning-examples/LearningComponent';
+import LearningJavaScript from './components/learning-examples/LearningJavaScript';
 
 // All components we wrote shoud be child components of App component
 function App() {
   return (
     <div className="App">
       <LearningComponent />
+      <LearningJavaScript />
     </div>
   );
 }
---
+++ Full-stack/frontend/todo-app/src/components/learning-examples/LearningJavaScript.jsx
@@ -0,0 +1,11 @@
+const person = {
+    name: 'Summer'
+}
+
+export default function LearningJavaScript() {
+    return (
+        <div>
+            {person.name}
+        </div>
+    );
+}
