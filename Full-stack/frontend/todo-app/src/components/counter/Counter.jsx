613179b02704d4248f6d72a6ee923c5185ff5b68
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-13 10:36:04 -0500

    create a counterbutton file

--- Full-stack/frontend/todo-app/src/components/counter/Counter.jsx
+++ Full-stack/frontend/todo-app/src/components/counter/Counter.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import { PropTypes } from 'prop-types'
 import './Counter.css'
+import CounterButton from './CounterButton'
 
 export default function Counter() {
 
@@ -22,42 +22,4 @@ export default function Counter() {
             <CounterButton by={5} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction} />
         </div>
     )
-}
-
-function CounterButton({ by, incrementMethod, decrementMethod }) {
-
-    // array
-    const [count, setCount] = useState(0);
-
-    function incrementCounterFunction() {
-        setCount(count + by)
-        incrementMethod(by)
-    }
-
-    function decrementCounterFunction() {
-        setCount(count - by)
-        decrementMethod(by)
-    }
-
-    return (
-        <div className="Counter">
-            <div>
-                <button className="counterButton"
-                    onClick={incrementCounterFunction}
-                >+{by}</button>
-                <button className="counterButton"
-                    onClick={decrementCounterFunction}
-                >-{by}</button>
-            </div>
-
-        </div>
-    )
-}
-
-CounterButton.propTypes = {
-    by: PropTypes.number
-}
-
-CounterButton.defaultProps = {
-    by: 1
 }
