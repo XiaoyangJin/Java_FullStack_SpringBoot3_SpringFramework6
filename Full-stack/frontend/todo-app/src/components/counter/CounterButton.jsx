dbb6cea2190ba5b420a8dd07a7228963db14d6c2
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-13 11:34:19 -0500

    arrow function

--- Full-stack/frontend/todo-app/src/components/counter/CounterButton.jsx
+++ Full-stack/frontend/todo-app/src/components/counter/CounterButton.jsx
@@ -2,9 +2,9 @@ import { PropTypes } from 'prop-types'
 
 export default function CounterButton({ by, incrementMethod, decrementMethod }) {
 
-    function incrementCounterFunction() {
-        incrementMethod(by)
-    }
+    // function incrementCounterFunction() {
+    //     incrementMethod(by)
+    // }
 
     function decrementCounterFunction() {
         decrementMethod(by)
@@ -14,7 +14,7 @@ export default function CounterButton({ by, incrementMethod, decrementMethod }) 
         <div className="Counter">
             <div>
                 <button className="counterButton"
-                    onClick={incrementCounterFunction}
+                    onClick={() => incrementMethod(by)}
                 >+{by}</button>
                 <button className="counterButton"
                     onClick={decrementCounterFunction}
