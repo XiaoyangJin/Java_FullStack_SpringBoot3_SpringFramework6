355bd0c8a3df31ec4e6f9ba4e6dcac848912a687
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-12 22:59:46 -0500

    try properties

--- Full-stack/frontend/todo-app/src/App.js
+++ Full-stack/frontend/todo-app/src/App.js
@@ -5,11 +5,24 @@ import Counter from './components/counter/Counter';
 function App() {
   return (
     <div className="App">
+      <PlayWithProps property1="value1" property2="value2"></PlayWithProps>
       <Counter />
       <Counter />
       <Counter />
+
     </div>
   );
+}
+
+function PlayWithProps(properties) {
+
+  console.log(properties);
+  console.log(properties.property1);
+  console.log(properties.property2);
+
+  return (
+    <div>Props</div>
+  )
 }
 
 export default App;
