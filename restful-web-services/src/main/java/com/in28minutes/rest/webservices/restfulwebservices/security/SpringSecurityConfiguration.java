a73af4a751cf44582ec0223f50a1da348591f8a7
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-12 22:55:05 -0500

    back to h2 version

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/security/SpringSecurityConfiguration.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/security/SpringSecurityConfiguration.java
@@ -6,19 +6,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringSecurityConfiguration {
 	
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		
-		//All requests should be authenticated
-		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
-		
-		//If a request is not authenticated, a web page is shown
-		http.httpBasic(withDefaults());
-		
-		//CSRF -> POST, PUT
-		http.csrf().disable();
-		
-		return http.build();
-	}
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//		
+//		//All requests should be authenticated
+//		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+//		
+//		//If a request is not authenticated, a web page is shown
+//		http.httpBasic(withDefaults());
+//		
+//		//CSRF -> POST, PUT
+//		http.csrf().disable();
+//		
+//		return http.build();
+//	}
 
 }
