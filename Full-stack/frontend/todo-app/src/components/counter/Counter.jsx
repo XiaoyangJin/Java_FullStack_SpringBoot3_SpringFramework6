db9fca9670f582ff0b690e8a2e3bb516fb5508b1
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-13 10:43:07 -0500

    reset button

--- Full-stack/frontend/todo-app/src/components/counter/Counter.jsx
+++ Full-stack/frontend/todo-app/src/components/counter/Counter.jsx
@@ -14,12 +14,19 @@ export default function Counter() {
         setCount(count - by)
     }
 
+    function resetCounter() {
+        return setCount(0);
+    }
+
     return (
         <div>
             <span className="totalCount">{count}</span>
             <CounterButton by={1} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction} />
             <CounterButton by={3} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction} />
             <CounterButton by={5} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction} />
+            <button className="resetButton"
+                onClick={resetCounter}
+            >Reset</button>
         </div>
     )
 }
