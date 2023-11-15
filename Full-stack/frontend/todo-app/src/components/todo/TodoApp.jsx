b62423b223b67e8f540ac60ed37228045f7e58cf
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-15 10:45:26 -0500

    prepare login component

--- Full-stack/frontend/todo-app/src/components/todo/TodoApp.jsx
+++ Full-stack/frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -2,6 +2,15 @@ export default function TodoApp() {
     return (
         <div className="TodoApp">
             Todo Management Application
+            <LoginComponent />
+        </div>
+    )
+}
+
+function LoginComponent() {
+    return (
+        <div className="Login">
+            Login Component
         </div>
     )
 }
