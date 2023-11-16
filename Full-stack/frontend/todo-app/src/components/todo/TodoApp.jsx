faa4e42444e8df805ee8785a20d1009b6387f174
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-15 21:11:58 -0500

    more paras

--- Full-stack/frontend/todo-app/src/components/todo/TodoApp.jsx
+++ Full-stack/frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -101,11 +101,14 @@ function ErrorComponent() {
 
 function ListTodosComponent() {
 
+    const today = new Date();
+    const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay())
+
     const todos = [
-        { id: 1, description: 'Learn AWS' },
-        { id: 2, description: 'Learn Full Stack Dev' },
-        { id: 3, description: 'Learn DevOps' },
-        { id: 4, description: 'Learn CSS' }
+        { id: 1, description: 'Learn AWS', done: false, targetDate: targetDate },
+        { id: 2, description: 'Learn Full Stack Dev', done: false, targetDate: targetDate },
+        { id: 3, description: 'Learn DevOps', done: false, targetDate: targetDate },
+        { id: 4, description: 'Learn CSS', done: false, targetDate: targetDate }
     ]
 
     return (
@@ -115,8 +118,10 @@ function ListTodosComponent() {
                 <table>
                     <thead>
                         <tr>
-                            <td>id</td>
-                            <td>description</td>
+                            <td>Id</td>
+                            <td>Description</td>
+                            <td>Is Done?</td>
+                            <td>Target Date</td>
                         </tr>
                     </thead>
                     <tbody>
@@ -126,6 +131,8 @@ function ListTodosComponent() {
                                     <tr key={todo.id}>
                                         <td>{todo.id}</td>
                                         <td>{todo.description}</td>
+                                        <td>{todo.done.toString()}</td>
+                                        <td>{todo.targetDate.toDateString()}</td>
                                     </tr>
                                 )
                             )
