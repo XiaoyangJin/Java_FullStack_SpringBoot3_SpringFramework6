b6ab6401195783225611b594434c77cc024299aa
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-18 22:37:18 -0500

    here we go

--- Full-stack/frontend/todo-app/src/components/todo/HeaderComponent.jsx
+++ Full-stack/frontend/todo-app/src/components/todo/HeaderComponent.jsx
@@ -6,7 +6,7 @@ export default function HeaderComponent() {
 
     const authContext = useContext(AuthContext)
 
-    console.log(authContext.value)
+    console.log(authContext.number)
 
     return (
         <header className="border-bottom border-light border-5 mb-5 p-2">
