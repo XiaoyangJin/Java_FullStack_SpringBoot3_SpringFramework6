3274bd0c69cafb1ac6e832c1cf950e10e114229b
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-18 22:41:54 -0500

    create a hook useAuth

--- Full-stack/frontend/todo-app/src/components/todo/security/AuthContext.js
+++ Full-stack/frontend/todo-app/src/components/todo/security/AuthContext.js
@@ -1,14 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 //1: Create a Context
 export const AuthContext = createContext()
 
-//3: Put some state in the context
+export const useAuth = () => useContext(AuthContext)
+
 
 //2: Share the created context with other component
 
 export default function AuthProvider({ children }) {
 
+    //3: Put some state in the context
     const [number, setNumber] = useState(10)
 
     setInterval(() => setNumber(number + 1), 10000)
