6aee55f5931cc4e5534a3bf6583b6eb601e483c8
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-21 16:43:52 -0500

    double check

--- Full-stack/frontend/todo-app/src/components/todo/WelcomeComponent.jsx
+++ Full-stack/frontend/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -24,7 +24,7 @@ export default function WelcomeComponent() {
 
     function successfulResponse(response) {
         console.log(response)
-        //setMessage(response.data)
+        setMessage(response.data.message)
     }
 
     function errorResponse(error) {
