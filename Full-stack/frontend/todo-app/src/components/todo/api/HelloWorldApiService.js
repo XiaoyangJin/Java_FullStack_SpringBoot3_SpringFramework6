d616c82e50942a6c0b6e11bd78087fa088e07017
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-21 16:50:38 -0500

    easier way

--- Full-stack/frontend/todo-app/src/components/todo/api/HelloWorldApiService.js
+++ Full-stack/frontend/todo-app/src/components/todo/api/HelloWorldApiService.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
-export function retrieveHelloWorldBean() {
-    return axios.get('http://localhost:8080/hello-world-bean')
-}
+// export function retrieveHelloWorldBean() {
+//     return axios.get('http://localhost:8080/hello-world-bean')
+// }
+
+export const retrieveHelloWorldBean = () => axios.get('http://localhost:8080/hello-world-bean')
