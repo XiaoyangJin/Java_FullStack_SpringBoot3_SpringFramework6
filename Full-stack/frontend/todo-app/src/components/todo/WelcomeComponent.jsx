9bd539c1576004fb77982199a33d2660bf72f921
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-20 23:31:33 -0500

    add a call rest api button

--- Full-stack/frontend/todo-app/src/components/todo/WelcomeComponent.jsx
+++ Full-stack/frontend/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -4,13 +4,18 @@ export default function WelcomeComponent() {
 
     const { username } = useParams()
 
-    console.log(username)
+    function callHelloWorldApi() {
+
+    }
 
     return (
         <div className="WelcomeComponent">
             <h1>Welcome {username}!</h1>
             <div>
                 Manage your todos - <Link to="/todos">Go here</Link>
+            </div>
+            <div>
+                <button class="btn btn-success" onClick={callHelloWorldApi}>Call Hello World REST API</button>
             </div>
         </div>
     )
