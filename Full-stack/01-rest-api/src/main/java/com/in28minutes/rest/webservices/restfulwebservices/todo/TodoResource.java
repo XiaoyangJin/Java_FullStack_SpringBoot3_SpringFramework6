00922f261a0bd5b99671a9c3755da9fa998fc219
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-26 17:19:59 -0500

    post

--- Full-stack/01-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ Full-stack/01-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +40,12 @@ public class TodoResource {
 	public Todo updateTodo(@PathVariable String username, @PathVariable int id, @RequestBody Todo todo) {
 		 todoService.updateTodo(todo);
 		 return todo;
+	}
+	
+	@PostMapping("/users/{username}/todos")
+	public Todo createTodo(@PathVariable String username, @RequestBody Todo todo) {
+		 Todo createdTodo = todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), todo.isDone());
+		 return createdTodo;
 	}
 
 }
