ae01c4087a853c7fffe46973c8cdd6bbf7e1af14
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-26 17:30:53 -0500

    change value and go back to todos page

--- Full-stack/frontend/todo-app/src/components/todo/TodoComponent.jsx
+++ Full-stack/frontend/todo-app/src/components/todo/TodoComponent.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { retrieveTodoApi } from './api/TodoApiService'
+import { useParams, useNavigate } from 'react-router-dom'
+import { retrieveTodoApi, updateTodoApi } from './api/TodoApiService'
 import { useAuth } from './security/AuthContext'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 
@@ -12,6 +12,7 @@ export default function TodoComponent() {
     const [targetDate, setTargetDate] = useState('')
 
     const authContext = useAuth()
+    const navigate = useNavigate()
     const username = authContext.username
     useEffect(
         () => retrieveTodos(),
@@ -25,14 +26,27 @@ export default function TodoComponent() {
                 setDescription(response.data.description)
                 setTargetDate(response.data.targetDate)
             }
-
             )
-
             .catch(error => console.log(error))
     }
 
     function onSubmit(values) {
         console.log(values)
+        const todo = {
+            id: id,
+            username: username,
+            description: values.description,
+            targetDate: values.targetDate,
+            done: false
+        }
+        console.log(todo)
+        updateTodoApi(username, id, todo)
+            .then(response => {
+                console.log(response)
+                navigate('/todos')
+            }
+            )
+            .catch(error => console.log(error))
     }
 
     function validate(values) {
--- Full-stack/frontend/todo-app/src/components/todo/api/TodoApiService.js
+++ Full-stack/frontend/todo-app/src/components/todo/api/TodoApiService.js
@@ -6,11 +6,12 @@ const apiClient = axios.create({
 
 export const retrieveAllTodosForUsernameApi =
     (username) => apiClient.get(`/users/${username}/todos`)
-// http://localhost:8080/users/xy/todos
 
 export const deleteTodoApi =
     (username, id) => apiClient.delete(`/users/${username}/todos/${id}`)
-// http://localhost:8080/users/xy/todos/1
+
 export const retrieveTodoApi =
     (username, id) => apiClient.get(`/users/${username}/todos/${id}`)
-    // http://localhost:8080/users/xy/todos/1
+
+export const updateTodoApi =
+    (username, id, todo) => apiClient.put(`/users/${username}/todos/${id}`, todo)
