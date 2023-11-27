b98a697fae29113ab12c526b5459315ab5f5ee22
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-27 10:41:03 -0500

    chain form

--- Full-stack/01-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
+++ Full-stack/01-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,9 +18,12 @@ public class BasicAuthenticationSecurityConfiguration {
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
 		http.authorizeHttpRequests(
-				auth -> auth.anyRequest().authenticated());
-		http.httpBasic(Customizer.withDefaults());
+				auth -> auth.anyRequest().authenticated())
+			.httpBasic(Customizer.withDefaults())
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.csrf().disable();
 		
 		return http.build();
 	}
