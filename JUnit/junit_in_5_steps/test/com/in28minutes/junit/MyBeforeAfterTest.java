38a369a220b1aaf3acb40c10c29f5ff80e0ff0fa
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-02 23:13:46 -0500

    one way to manage the sequnce

--- JUnit/junit_in_5_steps/test/com/in28minutes/junit/MyBeforeAfterTest.java
+++ JUnit/junit_in_5_steps/test/com/in28minutes/junit/MyBeforeAfterTest.java
@@ -2,9 +2,16 @@ package com.in28minutes.junit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MyBeforeAfterTest {
+	
+	@BeforeEach
+	void beforeEach() {
+		System.out.println("beforeEach");
+	}
 
 	@Test
 	void test1() {
@@ -19,6 +26,11 @@ class MyBeforeAfterTest {
 	@Test
 	void test3() {
 		System.out.println("test3");
+	}
+	
+	@AfterEach
+	void aftereEach() {
+		System.out.println("aftereEach");
 	}
 
 }
