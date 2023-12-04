6e2ee19b2b564b4694cc1cd41802b4f493c86a0d
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-04 08:53:07 -0500

    more fake data

--- Full-stack/01-rest-api/src/main/resources/data.sql
+++ Full-stack/01-rest-api/src/main/resources/data.sql
@@ -1,2 +1,8 @@
 insert into todo (id, description, done, target_date, username)
 values (10001, 'Learn draw', false, CURRENT_DATE(), 'xy');
+
+insert into todo (id, description, done, target_date, username)
+values (10002, 'Learn cook', false, CURRENT_DATE(), 'xy');
+
+insert into todo (id, description, done, target_date, username)
+values (10003, 'Buy milk', false, CURRENT_DATE(), 'xy');
