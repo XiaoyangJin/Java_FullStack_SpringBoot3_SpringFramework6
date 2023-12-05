c16389e8340c238ec508421c8f8de980d053af47
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-05 10:22:39 -0500

    generate a stub

--- Mockito/mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/business/SomeBusimessImplTest.java
+++ Mockito/mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/business/SomeBusimessImplTest.java
@@ -8,8 +8,20 @@ class SomeBusimessImplTest {
 
 	@Test
 	void test() {
-		SomeBusinessImpl businessImpl = new SomeBusinessImpl();
+		
+		DataServiceStub dataServiceStub = new DataServiceStub();
+		
+		SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceStub);
 		businessImpl.findTheGreatestFromAllData();
 	}
 
+}
+
+class DataServiceStub implements DataService{
+
+	@Override
+	public int[] retrieveAllData() {
+		return new int[]{25, 23, 14};
+	}
+	
 }
