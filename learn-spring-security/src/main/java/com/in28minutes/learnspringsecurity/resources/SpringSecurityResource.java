1dd75343fc97ef63f789d01446e96c23d2148fff
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-09 16:08:16 -0500

    Update SpringSecurityResource.java

--- learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/resources/SpringSecurityResource.java
+++ learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/resources/SpringSecurityResource.java
@@ -1,13 +1,16 @@
 package com.in28minutes.learnspringsecurity.resources;
 
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class SpringSecurityResource {
 	
-	@GetMapping("/hello-world")
-	public String helloWorld() {
-		return "Helo World v1";
+	@GetMapping("/csrf-token")
+	public CsrfToken retrieveCsrfToken(HttpServletRequest request) {
+		return (CsrfToken) request.getAttribute("_csrf");
 	}
 }
