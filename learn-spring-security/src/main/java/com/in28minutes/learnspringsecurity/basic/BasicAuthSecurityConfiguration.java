d7d9005c9cfd8eccb03d1a6b012bdc72752b118f
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-12 09:10:53 -0500

    BCryptPasswordEncoder

--- learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -78,5 +79,10 @@ public class BasicAuthSecurityConfiguration {
 		jdbcUserDetailsManager.createUser(admin);
 		
 		return jdbcUserDetailsManager;
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwodEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
