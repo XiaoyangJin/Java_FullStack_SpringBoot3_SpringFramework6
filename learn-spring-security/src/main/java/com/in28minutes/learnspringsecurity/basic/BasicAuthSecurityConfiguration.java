c74599cf891a4542cc0f109e2ab5c3bbc2f177ec
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-11 21:13:52 -0500

    add user and new dependencies

--- learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,6 +31,17 @@ public class BasicAuthSecurityConfiguration {
 	
 	@Bean
 	public UserDetailsService userDetailsService() {
-		return new InMemoryUserDetailsManager();
+		
+		var user = User.withUsername("in28minutes")
+					.password("{noop}dummy")
+					.roles("USER")
+					.build();
+		
+		var admin = User.withUsername("admin")
+				.password("{noop}dummy")
+				.roles("ADMIN")
+				.build();
+		
+		return new InMemoryUserDetailsManager(user, admin);
 	}
 }
