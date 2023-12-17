078b74642463921b9f3199f33c789e1702f2f097
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-16 21:37:17 -0500

    create jwt token and able to return it(hard coded one

--- learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/jwt/JwtAuthenticationResource.java
+++ learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/jwt/JwtAuthenticationResource.java
@@ -1,14 +1,24 @@
 package com.in28minutes.learnspringsecurity.jwt;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JwtAuthenticationResource {
 	
+	//create jwt token
+	private JwtEncoder jwtEncoder;
+	public JwtAuthenticationResource(JwtEncoder jwtEncoder) {
+		this.jwtEncoder = jwtEncoder;
+	}
+	
 	@PostMapping("/authenticate")
-	public Authentication authenticate(Authentication authentication) {
-		return authentication;
+	public JwtResponse authenticate(Authentication authentication) {
+		return new JwtResponse("token");
 	}
 }
+
+// able to return a response with token
+record JwtResponse(String token) {}
