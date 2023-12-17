e70548541367e731f6289c2d575e287267eb2c2a
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-16 21:45:16 -0500

    create token

--- learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/jwt/JwtAuthenticationResource.java
+++ learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/jwt/JwtAuthenticationResource.java
@@ -1,7 +1,12 @@
 package com.in28minutes.learnspringsecurity.jwt;
 
+import java.time.Instant;
+import java.util.stream.Collectors;
+
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +21,27 @@ public class JwtAuthenticationResource {
 	
 	@PostMapping("/authenticate")
 	public JwtResponse authenticate(Authentication authentication) {
-		return new JwtResponse("token");
+		return new JwtResponse(createToken(authentication));
+	}
+
+	// create a method that take the authentication object and create the jwt response
+	private String createToken(Authentication authentication) {
+		//encoder
+		var claims = JwtClaimsSet.builder()
+								.issuer("self")
+								.issuedAt(Instant.now())
+								.expiresAt(Instant.now().plusSeconds(60 * 30))
+								.subject(authentication.getName())
+								.claim("scope", createScope(authentication))
+								.build();
+		
+		return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+	}
+
+	private String createScope(Authentication authentication) {
+		return authentication.getAuthorities().stream()
+		.map(a -> a.getAuthority())
+		.collect(Collectors.joining(" "));
 	}
 }
 
