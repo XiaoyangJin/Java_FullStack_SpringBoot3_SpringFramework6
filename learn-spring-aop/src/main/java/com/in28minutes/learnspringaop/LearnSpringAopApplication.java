7eb31714fa2c614bb585aa01806dc1ded8fe7b2c
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-22 16:46:55 -0500

    log

--- learn-spring-aop/src/main/java/com/in28minutes/learnspringaop/LearnSpringAopApplication.java
+++ learn-spring-aop/src/main/java/com/in28minutes/learnspringaop/LearnSpringAopApplication.java
@@ -1,5 +1,7 @@
 package com.in28minutes.learnspringaop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +11,8 @@ import com.in28minutes.learnspringaop.aopexample.business.BusinessService1;
 @SpringBootApplication
 public class LearnSpringAopApplication implements CommandLineRunner {
 	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	private BusinessService1 businessService1;
 
 	public static void main(String[] args) {
@@ -17,6 +21,7 @@ public class LearnSpringAopApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		logger.info("Value returned is {}", businessService1.calculateMax());
 		businessService1.calculateMax();
 	}
 
