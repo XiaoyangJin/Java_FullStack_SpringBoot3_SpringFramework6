d41523d78078b0b506cdd2bf29db624bb6e54b63
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-27 00:22:02 -0500

    Define annotation

--- learn-spring-aop/src/main/java/com/in28minutes/learnspringaop/aopexample/data/DataService2.java
+++ learn-spring-aop/src/main/java/com/in28minutes/learnspringaop/aopexample/data/DataService2.java
@@ -2,9 +2,12 @@ package com.in28minutes.learnspringaop.aopexample.data;
 
 import org.springframework.stereotype.Repository;
 
+import com.in28minutes.learnspringaop.aopexample.annotations.TrackTime;
+
 @Repository
 public class DataService2 {
 
+	@TrackTime
 	public int[] retrieveData() {
 		try {
 			Thread.sleep(30);
