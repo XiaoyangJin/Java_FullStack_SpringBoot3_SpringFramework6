f8f194fa9848f2b38352c6bd60c1565e79d75c41
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-29 22:42:52 -0500

    able to run the application

--- learn-docker/src/main/java/com/in28minutes/learndocker/HelloWorldController.java
+++ learn-docker/src/main/java/com/in28minutes/learndocker/HelloWorldController.java
@@ -1,7 +1,9 @@
 package com.in28minutes.learndocker;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloWorldController {
 
 	@GetMapping(path = "/")
